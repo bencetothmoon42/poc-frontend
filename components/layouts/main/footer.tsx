da1ac13e46374aeb44fcf644a0817d7d3843b036
CsengeFeher <csenge.feher@moon42.com>
2022-10-14 13:49:52 +0200

    PNC-11: change footer to fixed

--- components/layouts/main/footer.tsx
+++ components/layouts/main/footer.tsx
@@ -6,8 +6,8 @@ type Props = {
 export default function Footer({updateDate, versionNumber}: Props) {
 
   return (
-    <footer className="flex justify-center h-[4.25rem] border-t-2 border-pnc-grey mx-[-20%] mb-[24px]">
-         <div className="text-base tracking-wide w-[62rem] mx-auto">
+    <footer className="fixed bottom-0 bg-white w-[62rem] px-2 pb-6 border-t-2 border-pnc-grey">
+         <div className="text-base tracking-wide">
              <p className="uppercase underline  mb-1.5 mt-3.5 ">software version {versionNumber}.</p>
              <p>last updated: {updateDate}</p>
          </div>
--- components/layouts/main/index.tsx
+++ components/layouts/main/index.tsx
@@ -8,7 +8,7 @@ export default function MainLayout(props: { children: React.ReactNode }) {
   return (
       <div className="w-[62rem] mx-auto">
         <Header />
-        <main className="pb-16">{props.children}</main>
+        <main className="pb-32">{props.children}</main>
         <Footer 
           updateDate={updateDate}
           versionNumber={versionNumber}
